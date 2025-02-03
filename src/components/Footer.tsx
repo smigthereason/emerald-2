9dd4ea5ec6a5560d7f0ce8e0fde16b9005a11aae
Victor <victor.dmaina@gmail.com>
2025-02-03 12:41:51 +0300

    fix footer elements, remove overflow

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
   return (
-    <footer className=" w-screen text-black text-2xl overflow-x-hidden p-4">
+    <footer className="w-screen text-black font-light text-2xl overflow-x-hidden p-4 ">
       <div className="px-4 py-10">
-        <div className="flex flex-col md:flex-row md:justify-between items-start">
-          {/* Logo and tagline */}
-          <div className="w-full sm:w-1/2 md:w-1/5 mb-6 md:mb-0 flex justify-center md:justify-start">
+        <div className="flex flex-col md:flex-row md:justify-between items-start gap-4">
+          {/* Logo */}
+          <div className="w-full md:w-1/6 flex justify-center md:justify-start mb-6 md:mb-0">
             <img
               className="h-auto w-48"
               src="/src/assets/Logos/e-logo1.png"
@@ -15,121 +15,117 @@ const Footer: React.FC = () => {
             />
           </div>
 
-          {/* About */}
-          <div className="w-full sm:w-1/2 md:w-1/5 mb-6 md:mb-0">
-            <h3 className="font-bold mb-2 text-center md:text-left">About</h3>
-            <ul className="text-xl space-y-1 text-center md:text-left">
-              <li>
-                <Link to="/" className="no-underline hover:text-[#ff6633]">
-                  Our Mission
+          {/* Shop by Category */}
+          <div className="w-full md:w-1/6 mb-6 md:mb-0">
+            <ul className="text-xl space-y-1 text-center md:text-left ">
+              <li className="transform transition duration-300 hover:scale-95">
+                <Link
+                  to="/new-arrivals"
+                  className="no-underline hover:text-[#ff6633] "
+                >
+                  New Arrivals
                 </Link>
               </li>
-              <li>
-                <Link to="/" className="no-underline hover:text-[#ff6633]">
-                  Team
+              <li className="transform transition duration-300 hover:scale-95">
+                <Link to="/sale" className="no-underline hover:text-[#ff6633]">
+                  Sale &amp; Clearance
                 </Link>
               </li>
-              <li>
-                <Link to="/" className="no-underline hover:text-[#ff6633]">
-                  Beneficiary Stories
+              <li className="transform transition duration-300 hover:scale-95">
+                <Link
+                  to="/about-us"
+                  className="no-underline hover:text-[#ff6633]"
+                >
+                  About Us
                 </Link>
               </li>
             </ul>
           </div>
 
-          {/* Get Involved */}
-          <div className="w-full sm:w-1/2 md:w-1/5 mb-6 md:mb-0">
-            <h3 className="font-bold mb-2 text-center md:text-left">
-              Get Involved
-            </h3>
+          {/* Support & Resources */}
+          <div className="w-full md:w-1/6 mb-6 md:mb-0">
             <ul className="text-xl space-y-1 text-center md:text-left">
-              <li>
-                <Link to="/loading" className="no-underline hover:text-[#ff6633]">
-                  Donate
-                </Link>
-              </li>
-              <li>
-                <Link to="/partner" className="no-underline hover:text-[#ff6633]">
-                  Partner
-                </Link>
-              </li>
-              <li>
+              <li className="transform transition duration-300 hover:scale-95">
                 <Link
-                  to="/volunteer"
+                  to="/size-guide"
                   className="no-underline hover:text-[#ff6633]"
                 >
-                  Volunteer
+                  Size Guide
+                </Link>
+              </li>
+              <li className="transform transition duration-300 hover:scale-95">
+                <Link to="/faq" className="no-underline hover:text-[#ff6633]">
+                  FAQs
+                </Link>
+              </li>
+              <li className="transform transition duration-300 hover:scale-95">
+                <Link
+                  to="/store-locator"
+                  className="no-underline hover:text-[#ff6633]"
+                >
+                  Store Locator
+                </Link>
+              </li>
+              <li className="transform transition duration-300 hover:scale-95">
+                <Link
+                  to="/gift-cards"
+                  className="no-underline hover:text-[#ff6633]"
+                >
+                  Gift Cards
                 </Link>
               </li>
             </ul>
           </div>
 
-          {/* Support */}
-          <div className="w-full sm:w-1/2 md:w-1/5 mb-6 md:mb-0">
-            <h3 className="font-bold mb-2 text-center md:text-left">Support</h3>
+          {/* Social Networks */}
+          <div className="w-full md:w-1/6 mb-6 md:mb-0">
             <ul className="text-xl space-y-1 text-center md:text-left">
-              <li>
-                <Link
-                  to="/contact#contact"
+              <li className="transform transition duration-300 hover:scale-95">
+                <a
+                  href="https://instagram.com"
                   className="no-underline hover:text-[#ff6633]"
-                >
-                  Feedback
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/policies#policy"
-                  className="no-underline hover:text-[#ff6633]"
-                >
-                  Policies
-                </Link>
-              </li>
-              <li>
-                <Link to="/FAQ#faq" className="no-underline hover:text-[#ff6633]">
-                  F.A.Q
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          {/* Social Network */}
-          <div className="w-full sm:w-1/2 md:w-1/5 mb-6 md:mb-0">
-            <h3 className="font-bold mb-2 text-center md:text-left">
-              Social Network
-            </h3>
-            <ul className="text-xl space-y-1 text-center md:text-left">
-              <li>
-                <Link
-                  to="https://facebook.com"
-                  className="no-underline hover:text-[#ff6633]"
-                >
-                  Facebook
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="https://instagram.com"
-                  className="no-underline hover:text-[#ff6633]"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   Instagram
-                </Link>
+                </a>
               </li>
-              <li>
-                <Link
-                  to="https://tiktok.com"
-                  className="no-underline hover:text-[#ff6633]"
+              <li className="transform transition duration-300 hover:scale-95">
+                <a
+                  href="https://tiktok.com"
+                  className="no-underline hover:text-[#ff6633] "
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   TikTok
-                </Link>
+                </a>
               </li>
             </ul>
           </div>
         </div>
 
         {/* Copyright */}
-        <div className="mt-8 flex justify-center">
-          <div className="w-full text-center text-sm text-gray-400">
-            Copyright © All Rights Reserved
+        <div className="mt-8  pt-4">
+          <div className="flex flex-col md:flex-row justify-center items-center gap-4">
+            <p className="text-sm text-gray-400">
+              Copyright © All Rights Reserved
+            </p>
+            <p className="text-sm text-gray-400 transform transition duration-300 hover:scale-110">
+              <Link
+                to="/privacy-policy"
+                className="no-underline hover:text-[#352f2d]"
+              >
+                Privacy Policy
+              </Link>
+            </p>
+            <p className="text-sm text-gray-400 transform transition duration-300 hover:scale-110">
+              <Link
+                to="/terms-of-service"
+                className="no-underline hover:text-[#352f2d]"
+              >
+                Terms of Service
+              </Link>
+            </p>
           </div>
         </div>
       </div>
