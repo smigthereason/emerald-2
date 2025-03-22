0b4fa97bbb03f2eb34c0c4753b86a094ff7398a6
Victor <victor.dmaina@gmail.com>
2025-03-22 13:16:25 +0300

    fix tailwind dev dependency

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,6 +1,7 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
@@ -8,6 +9,5 @@ module.exports = {
       },
     },
   },
-
-  plugins: [require("tailwind-scrollbar"),],
-};
+  plugins: [require("tailwind-scrollbar")],
+}
